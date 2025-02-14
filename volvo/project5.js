8184901fa94e271fe77e2c9c002a7d5a47a4c696
sami <samikaya89@gmail.com>
2025-02-14 22:18:41 +0300

    Update project5.js

--- volvo/project5.js
+++ volvo/project5.js
@@ -1,1 +1,40 @@
 
+// Volvo'da developer olarak işe başladın. Takas desteği ile gelen arabaları web sitesinde yayınlamak istiyorlar. Takım olarak işleri paylaştınız ve senden özelliklere göre araba nesnesi oluşturan bir fonksiyon yazmanı istediler.
+// Bunun için adı getCarObject olan bir fonksiyon yazalım.
+// Bu fonksiyon bir arabanın sırasıyla id, model, yakit, vites, yas, fiyat, kilometre değerlerini parametre olarak alsın.
+// Gelen bu değerler ile üretim yılını hesaplasın ve id, model, yakit, vites, uretimYili, fiyat, kilometre bilgileri olan bir objeyi geri dönsün.
+
+
+// Örnek Kullanım:
+getCarObject(121, 'VW Passat', 'Benzin', 'Otomatik', 11, 800000, 153000);
+
+// {
+// 	id: 121,
+// 	model: 'VW Passat',
+// 	yakit: 'Benzin',
+// 	vites: 'Otomatik',
+// 	uretimYili: 2012,
+// 	fiyat: 800000,
+// 	kilometre: 153000
+// }
+
+// İpucu: boş bir obje oluşturup, istenenleri key-value ikilileri olarak içine ekleyebilirsin.
+// İpucu: getFullYear() metodu ile bulunduğumuz yılı dinamik olarak alabilirsin. Nasıl kullanıldığını w3schools'tan araştırabilirsin.
+// Dikkat
+// Fonksiyon yas değerini alıp üretim yılını geri dönmeli!
+
+
+function getCarObject(id , model, yakit, vites, yas, fiyat, kilometre) {
+    const getObject = {};
+    getObject.id = id;
+    getObject.model = model;
+    getObject.yakit = yakit;
+    getObject.vites = vites;
+    getObject.uretimyili = new Date().getFullYear() - yas;
+    getObject.fiyat = fiyat;
+    getObject.kilometre = kilometre;
+    return getObject
+}
+
+
+console.log(getCarObject(121, 'VW Passat', 'Benzin', 'Otomatik', 11, 800000, 153000));
